bf8f31df1fa9e430776d0dd09b7359fba5c445a2
cadart <cadart.valentin@sfr.fr>
2023-05-24 09:52:12 +0200

    add User Class

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,21 +1,17 @@
 import { Component } from '@angular/core';
-
+import { User } from './User';
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent {
-  user = {
-    name : 'Doe',
-    firstName : 'John',
-    age : 25,
-    quote : '',
-    photo : 'https://randomuser.me/api/portraits/lego/2.jpg',
-    hide : true
-  };
-
-
+  user: User;
+  
+  constructor() {
+    this.user = new User("Doe", "John", 25, "https://randomuser.me/api/portraits/lego/2.jpg");
+  }
+ 
   toggle() {
     this.user.hide = !this.user.hide;
   }
