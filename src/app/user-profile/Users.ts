b4b5f82dfd35498ed85faa260bc55ffa9da133fc
cadart <cadart.valentin@sfr.fr>
2023-05-24 10:01:33 +0200

    add Users

--- src/app/user-profile/Users.ts
+++ src/app/user-profile/Users.ts
@@ -1,4 +1,4 @@
-export class User {
+export class Users {
     name: string;
     firstName: string;
     age: number;
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { User } from './User';
+import { Users } from './Users';
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent {
-  user: User;
-  
+  user: Users;
+
   constructor() {
-    this.user = new User("Doe", "John", 25, "https://randomuser.me/api/portraits/lego/2.jpg");
+    this.user = new Users("Doe", "John", 25, "https://randomuser.me/api/portraits/lego/2.jpg");
   }
  
   toggle() {
